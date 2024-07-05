83db31b9e897d55cb7460f109bda0a95afc89139
Ammar Tariq <146677780+iammartariq@users.noreply.github.com>
2024-07-06 01:21:50 +0500

    Update Voice Assistant.py

--- Voice Assistant.py
+++ Voice Assistant.py
@@ -30,7 +30,7 @@ def listen():
 
         except sr.UnknownValueError:
 
-            print("Sorry, Can you please repeat? I could'nt understand that.")
+            print("Sorry, Can you please repeat? I couldn't understand that.")
             speak("Sorry, Can you please repeat? I could not understand that.")
 
             return None
@@ -47,28 +47,52 @@ def listen():
 def handle_query(query):
 
     if 'hello' in query or 'hi' in query or 'hey' in query:
+
         speak("Hello! How can I help you?")
         speak("What are your plans for today?")
+        print("Hello! How can I help you?")
+        print("What are your plans for today?")
 
     elif 'how are you' in query or "how r you" in query or "how r" in query:
+
         speak("I am good, thankyou for asking!! How are you?")
+        print("I am good, thankyou for asking!! How are you?")
 
     elif 'I am good' in query or 'I am fine' in query:
+
         speak("Glad to hear that!")
         speak("What are your plans for today?")
+        print("Glad to hear that!")
+        print("What are your plans for today?")
 
     elif 'time' in query:
+
         now = datetime.now().strftime("%H:%M")
+
         speak(f"The current time is {now}")
         speak("What are your plans for today?")
+        print(f"The current time is {now}")
+        print("What are your plans for today?")
 
     elif 'search' or 'look' or 'look for' or 'find' in query:
+
         query = query.replace("search", "").replace("look", "").replace("look for","").replace("find","")
         webbrowser.open(f"https://www.google.com/search?q={query}")
-        speak(f"Here are results for {query.strip()}, i hope that will help you out")
 
-    else:
-        speak("I'm sorry, I didn't understand that. Can you please elaborate?")
+        speak(f"Here are results for {query.strip()}, i hope that will help you out")
+        print(f"Here are results for {query.strip()}, i hope that will help you out")
+
+    elif "stop" in query or "end" in query or "finish" in query:
+
+        speak("Goodnbye! Have a nice day!!")
+        print("Goodbye! Have a nice day!!")
+
+        return False
+    
+    speak("What else can I help you with?")
+    print("What else can I help you with?")
+
+    return True
 
 if __name__ == "__main__":
 
@@ -80,9 +104,5 @@ if __name__ == "__main__":
         query = listen()
         
         if query:
+            
             handle_query(query)
-
-            if "stop" in query or "end" in query or "finish" in query:
-                speak("Goodbye! Have a nice day!!")
-                
-                break
